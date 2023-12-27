6e6790f376b0cb97d8dd78d3239188966b06f350
nieberlen <nieberlen@msoe.edu>
2023-12-27 02:39:01 -0600

    Questions are displayed once loaded, and user can answer them. When they are submitted they are presented with a total score screen.

--- src/Home.js
+++ src/Home.js
@@ -4,37 +4,52 @@ import {useState} from "react";
 export const Home = () => {
 
     const [questionsSet, setQuestionsSet] = useState(false);
-    const [difficulty, setDifficulty] = useState("Easy");
-    const [questionType, setQuestionType] = useState("TrueFalse");
+    const [difficulty, setDifficulty] = useState("None");
+    const [questionType, setQuestionType] = useState("None");
     const [category, setCategory] = useState("None");
-    const [numberOfQuestions, setNumberOfQuestions] = useState(10);
+    const [numberOfQuestions, setNumberOfQuestions] = useState(-1);
     const [isError, setIsError] = useState(false);
+    const [questions, setQuestions] = useState([]);
+    const [answeredQuestions, setAnsweredQuestion] = useState({})
+    const [quizIsDone, setQuizIsDone] = useState(false)
+    const [quizScore, setQuizScore] = useState(0)
+    const [correctQuestions, setCorrectQuestions] = useState([])
+    const [incorrectQuestions, setIncorrectQuestions] = useState([])
+
 
     function handleSubmit() {
-        console.log(difficulty)
-        console.log(questionType)
-        console.log(category)
-        console.log(numberOfQuestions)
 
+
+
+
+        const request = "https://opentdb.com/api.php?amount=" + numberOfQuestions + "&category=" + category + "&difficulty=" + difficulty + "&type=" + questionType
         fetch('https://opentdb.com/api.php?amount=10&category=9&difficulty=easy&type=multiple').then(
-            async response => {
-                response = await response.json()
-                if (response.response_code === 0) {
-                    localStorage.setItem("questions",JSON.stringify(response.results))
-                    let questions = JSON.parse(localStorage.getItem("questions"))
+            async responses => {
+                responses = await responses.json()
+                if (responses.response_code === 0) {
 
                     if(!questionsSet) {
                         setQuestionsSet(true)
                     }
-                } else if (response.response_code === 1) {
+                    for (let i = 0; i < responses.results.length; i++) {
+                        const currentResponse = responses.results[i]
+                        const correctAndIncorrectAnswers = currentResponse.incorrect_answers.concat(currentResponse.correct_answer)
+                        for (let i = correctAndIncorrectAnswers.length - 1; i > 0; i--) {
+                            const j = Math.floor(Math.random() * (i + 1));
+                            [correctAndIncorrectAnswers[i], correctAndIncorrectAnswers[j]] = [correctAndIncorrectAnswers[j], correctAndIncorrectAnswers[i]];
+                        }
+                        currentResponse.correctAndIncorrectAnswers = correctAndIncorrectAnswers
+                    }
+                    setQuestions(responses.results)
+                } else if (responses.response_code === 1) {
                     throw new Error("No Results Could not return results. The API doesn't have enough questions for your query. (Ex. Asking for 50 Questions in a Category that only has 20.)")
-                } else if (response.response_code === 2) {
+                } else if (responses.response_code === 2) {
                     throw new Error("Invalid Parameter Contains an invalid parameter. Arguements passed in aren't valid. (Ex. Amount = Five)")
-                } else if (response.response_code === 3) {
+                } else if (responses.response_code === 3) {
                     throw new Error("Token Not Found Session Token does not exist.")
-                } else if (response.response_code === 4) {
+                } else if (responses.response_code === 4) {
                     throw new Error("Token Empty Session Token has returned all possible questions for the specified query. Resetting the Token is necessary.")
-                } else if (response.response_code === 5) {
+                } else if (responses.response_code === 5) {
                     throw new Error("Rate Limit Too many requests have occurred. Each IP can only access the API once every 5 seconds.")
                 }
             }
@@ -45,6 +60,44 @@ export const Home = () => {
 
 
 
+    }
+
+    function handleAnswerQuestion(question,answer) {
+
+        for (let i = 0; i < questions.length;i++) {
+            if (questions[i].question === question) {
+                questions[i].userAnswer = answer
+            }
+        }
+       // answeredQuestions[question] = e
+    }
+    function handleSubmitQuestions() {
+        let correct_count = 0
+        const incorrectQuestions = []
+        const correctQuestions = []
+        for (let i = 0; i < questions.length; i++) {
+            if (questions[i].userAnswer === questions[i].correct_answer) {
+                correct_count += 1
+                correctQuestions.push([questions[i].question, questions[i].correct_answer])
+            } else {
+                incorrectQuestions.push([questions[i].question, questions[i].userAnswer])
+            }
+        }
+        const quizScore = (correct_count / questions.length) * 100
+        setQuizScore(quizScore)
+        setQuizIsDone(true)
+        setCorrectQuestions(correctQuestions)
+        setIncorrectQuestions(incorrectQuestions)
+
+        // answeredQuestions[question] = e
+    }
+
+    function endQuiz() {
+        setQuestions([])
+        setIncorrectQuestions([])
+        setCorrectQuestions()
+        setQuestionsSet(false)
+        setQuizIsDone(false)
     }
 
     return (<div><Navigation/>
@@ -66,6 +119,8 @@ export const Home = () => {
                 <option value="medium">Medium</option>
                 <option value="hard">Hard</option>
             </select>
+            <b>Number of questions</b>
+            <input type="text"></input>
             <b>Question Type</b>
             <div className="form-check" onChange={e => setQuestionType(e.target.id)}>
                 <input type={"radio"} className = "form-check-input" name="questionTypeSelection" id = "trueFalse"></input>
@@ -77,7 +132,27 @@ export const Home = () => {
             </div>
             <input type="submit" onClick={handleSubmit}/>
         </div>)}
-        {questionsSet && (<div className={"HomePage"}><p>Hello from home</p></div>)}
+        {questionsSet && (<div className={"HomePage"}>
+            <b>Quiz</b>
+            {questions.map(question => <div><b>{question.question}</b>{question.correctAndIncorrectAnswers.map(answer =>
+                <div  onChange={e => handleAnswerQuestion(question.question,e.target.id)}>
+                    <input type={"radio"} name={question.question} id = {answer} ></input>
+                    <label>{answer}</label>
+                </div>)}
+                </div>)}
+        <input type={"submit"} onClick={handleSubmitQuestions}/>
+        </div>)} {quizIsDone && (
+            <div className="endgame_overlay">
+                <b>You got {quizScore}% of the questions correct</b>
+                <br/>
+                <b>Correct Questions:</b>
+                {correctQuestions.map(question => <div><i>{question}</i> <br/></div>)}
+                <br/>
+                <b>Incorrect Questions (Your answer shown alongside):</b>
+                {incorrectQuestions.map(question => <div><i>{question}</i><br/></div>)}
+                <button onClick={endQuiz} className={"btn btn-primary"}>Done Reviewing Feedback</button>
+            </div>
+        )}
 
     </div>)
 }
